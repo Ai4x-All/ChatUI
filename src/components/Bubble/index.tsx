f0cea35e68556ea7d3fd895eaa55a05e6154c510
zhangkaiyue <15718842591@163.com>
2024-11-07 16:11:14 +0800

    fix(config): 更新组件

--- src/components/Bubble/index.tsx
+++ src/components/Bubble/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export interface BubbleProps extends React.HTMLAttributes<HTMLDivElement> {
   type?: string;
-  customContent?: React.ReactNode; // 重命名为 customContent
+  content?: string | React.ReactNode | undefined | null | any; // content 的类型允许 string 或 ReactNode
 }
 
 export const Bubble = React.forwardRef<HTMLDivElement, BubbleProps>((props, ref) => {
-  const { type = 'text', customContent, children, ...other } = props;
+  const { type = 'text', content, children, ...other } = props;
   return (
     <div className={`Bubble ${type}`} data-type={type} ref={ref} {...other}>
-      {customContent && <p>{customContent}</p>}
+      {content && <div className="content">{content}</div>} {/* 这里用 div 包裹 content */}
       {children}
     </div>
   );
