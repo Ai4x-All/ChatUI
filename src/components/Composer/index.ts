fc9c286280503e20196b086f0b7810d984aa9536
zhangkaiyue <zhangkaiyue@ai4x.com.cn>
2024-11-25 17:35:19 +0800

    fix: 更新

--- src/components/Composer/index.ts
+++ src/components/Composer/index.ts
@@ -1,2 +1,7 @@
 export { Composer } from './Composer';
 export type { ComposerProps, ComposerHandle } from './Composer';
+export { ComposerInput } from './ComposerInput';
+export { AccessoryWrap } from './AccessoryWrap';
+export { Action } from './Action';
+export { SendButton } from './SendButton';
+export { ToolbarItem } from './ToolbarItem';
