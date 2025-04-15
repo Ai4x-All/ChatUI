53cad3a95ff26d76800f27224468199477dbbe30
zhangkaiyue0318 <zhangkaiyue@ai4x.com.cn>
2025-04-15 11:32:46 +0800

    fix: isIOS 判断兼容小程序

--- src/utils/ua.ts
+++ src/utils/ua.ts
@@ -1,6 +1,16 @@
 const ua = navigator.userAgent;
+// 判断是否处于微信小程序环境
+export const isWxEnv = typeof window !== 'undefined' && window.__wxjs_environment === 'miniprogram';
 
-export const isIOS = /iPad|iPhone|iPod/.test(ua);
+// export const isIOS = /iPad|iPhone|iPod/.test(ua);
+let ios = /iPad|iPhone|iPod/.test(ua);
+// 针对 iOS 的判断：如果在微信小程序中，建议使用 wx.getSystemInfoSync 获取
+if (isWxEnv) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const platform = urlParams.get('platform');
+  ios = !!(platform && platform === 'ios');
+}
+export const isIOS = ios;
 
 export const isSafari = /^((?!chrome|android|crios|fxios).)*safari/i.test(ua);
 
